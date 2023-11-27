0ef75bead106c5becbfcb4f51dbb974f3c2c0fd0
AliHassan487 <ah0720487@gmail.com>
2023-11-27 15:43:27 +0500

    Add simple authentication logic and create user, admin, and manager pages

--- src/Pages/AdminPage/AdminPage.jsx
+++ src/Pages/AdminPage/AdminPage.jsx
@@ -1,1 +1,12 @@
+import React from 'react'
+
+const AdminPage = () => {
+  return (
+    <div>
+      <h1>Welcome on Admin page</h1>
+    </div>
+  )
+}
+
+export default AdminPage;
 
--- src/Pages/UserPage/UserPage.jsx
+++ src/Pages/UserPage/UserPage.jsx
@@ -1,1 +1,12 @@
+import React from 'react'
+
+const UserPage = () => {
+  return (
+    <div>
+      <h1>Welcome on user page </h1>
+    </div>
+  )
+}
+
+export default UserPage;
 
