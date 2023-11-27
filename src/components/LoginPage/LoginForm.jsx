09c987be3f241c40f8141618d2fea13f017d13cd
AliHassan487 <ah0720487@gmail.com>
2023-11-27 15:39:22 +0500

    Implement logic and basic login form

--- src/components/LoginPage/LoginForm.jsx
+++ src/components/LoginPage/LoginForm.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+// components/LoginPage/LoginForm.js
 
-const LoginForm = () => {
+import React, { useState } from 'react';
+
+const LoginForm = ({ onLogin }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    if (username === 'user' && password === 'userpass') {
+      onLogin('user');
+    } else if (username === 'admin' && password === 'adminpass') {
+      onLogin('admin');
+    } else if (username === 'manager' && password === 'managerpass') {
+      onLogin('manager');
+    } else {
+      alert('Invalid credentials');
+    }
+  };
+
   return (
     <div>
-      
+      <label>Username:</label>
+      <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+      <br />
+      <label>Password:</label>
+      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <br />
+      <button onClick={handleLogin}>Login</button>
     </div>
-  )
-}
+  );
+};
 
 export default LoginForm;
