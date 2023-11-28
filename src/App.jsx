217c689e6c2de4f4942ab233d407bee165b1833c
AliHassan487 <ah0720487@gmail.com>
2023-11-28 10:53:03 +0500

    style add in login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import { useState } from 'react'
 import './App.css'
 import LoginForm from './components/LoginPage/LoginForm'
+import UserPage from './Pages/UserPage/UserPage'
+import AdminPage from './Pages/AdminPage/AdminPage'
+import ManagerPage from './Pages/ManagerPage/ManagerPage'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loggedInUser, setLoggedInUser] = useState(null);
+
+  const handleLogin = (userType) => {
+    setLoggedInUser(userType);
+  };
 
   return (
     <>
-      <LoginForm/>
+       <div>
+      {loggedInUser ? (
+        <>
+          {loggedInUser === 'user' && <UserPage />}
+          {loggedInUser === 'admin' && <AdminPage />}
+          {loggedInUser === 'manager' && <ManagerPage />}
+        </>
+      ) : (
+        <LoginForm onLogin={handleLogin} />
+      )}
+    </div>
     </>
   )
 }
--- src/components/LoginPage/LoginForm.jsx
+++ src/components/LoginPage/LoginForm.jsx
@@ -19,16 +19,44 @@ const LoginForm = ({ onLogin }) => {
   };
 
   return (
-    <div>
-      <label>Username:</label>
-      <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-      <br />
-      <label>Password:</label>
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      <br />
-      <button onClick={handleLogin}>Login</button>
-    </div>
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+    <form className="bg-white p-8 rounded shadow-md">
+      <div className="mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+          Username:
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="username"
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div className="mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+          Password:
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div className="flex items-center justify-between">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="button"
+          onClick={handleLogin}
+        >
+          Login
+        </button>
+      </div>
+    </form>
+  </div>
   );
 };
 
-export default LoginForm;
+ export default LoginForm;
